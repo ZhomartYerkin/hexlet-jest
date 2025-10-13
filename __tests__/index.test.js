949f0449915958adbeb344c04ffd65969df1090f
Zhomart Yerkin <zyerkin@gmail.com>
2025-10-13 23:36:56 +0500

    test: adjust index.test.js (demo/fix)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,5 +2,5 @@ import reverse from '../src/index.js'
 
 test('reverse', () => {
   expect(reverse('hello')).toEqual('olleh')
-  expect(reverse('')).toEqual('abc')
+  expect(reverse('')).toEqual('')
 })
